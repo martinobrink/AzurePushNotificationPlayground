c55cc99104a9f3885019d72b744196f842e611c2
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-12-09 22:51:52 +0100

    Initial and very simple client code for Android app. Currently capable of receiving registration id from GCM.

---
+++ Clients/Android/PushNotificationDemo/App/src/main/java/com/dgsoft/pushnotificationdemo/MainActivity.java
@@ -0,0 +1,122 @@
+package com.dgsoft.pushnotificationdemo;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import android.content.Context;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    //SENDER_ID below: Please create your own senderid/projectnumber using google apis console instead of using mine :)
+    String SENDER_ID = "337436325121";
+    private static final String TAG = "GCMDemo";
+    private GoogleCloudMessaging gcm;
+
+    TextView regIdTextView;
+    Context context;
+    String regid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        regIdTextView = (TextView) findViewById(R.id.regIdTextView);
+        context = getApplicationContext();
+
+        gcm = GoogleCloudMessaging.getInstance(this);
+
+        new RegisterBackground().execute();
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    class RegisterBackground extends AsyncTask<String,String,String> {
+
+        @Override
+        protected String doInBackground(String... arg0) {
+            // TODO Auto-generated method stub
+            String msg = "";
+            try {
+                if (gcm == null) {
+                    gcm = GoogleCloudMessaging.getInstance(context);
+                }
+                regid = gcm.register(SENDER_ID);
+                msg = "Device registered, registration ID=" + regid;
+                Log.d("111", msg);
+                sendRegistrationIdToBackend(regid);
+
+            } catch (IOException ex) {
+                msg = "Error :" + ex.getMessage();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg) {
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+            //regIdTextView.append(msg + "\n");
+        }
+    private void sendRegistrationIdToBackend(String regid) {
+        Log.i(TAG, regid);
+        // this code will send registration id of a device to our own server.
+    }}
+}
