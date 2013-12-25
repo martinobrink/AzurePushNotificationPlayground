0e6369fa16bbf12d9b44eb68e78dbc5809883d7b
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2013-12-25 16:20:18 +0100

    implemented fix in web api backend so that re-registration of same device updates instead of creating another device

--- Backends/PushNotificationDemo.WebApiBackend/Controllers/DeviceController.cs
+++ Backends/PushNotificationDemo.WebApiBackend/Controllers/DeviceController.cs
@@ -9,8 +9,6 @@ namespace PushNotificationDemo.WebApiBackend.Controllers
 {
     public class DeviceController : BaseController
     {
-        private static readonly Dictionary<string, Device> _devices = new Dictionary<string, Device>();
-
         // GET api/device
         public Task<IList<Device>> Get()
         {
@@ -18,12 +16,11 @@ namespace PushNotificationDemo.WebApiBackend.Controllers
         }
 
         // GET api/device/44809e3f-ebe0-4bdf-8aaf-2c6581496ef2
-        public IHttpActionResult Get(string deviceGuid)
+        public async Task<IHttpActionResult> Get(string deviceGuid)
         {
-            Device device;
-            var deviceFound = _devices.TryGetValue(deviceGuid, out device);
+            var device = await Session.Query<Device>().FirstOrDefaultAsync(d => d.DeviceGuid == deviceGuid);
 
-            if (deviceFound)
+            if (device != null)
             {
                 return Ok(device);
             }
@@ -34,16 +31,24 @@ namespace PushNotificationDemo.WebApiBackend.Controllers
         // PUT api/device
         public async Task<IHttpActionResult> Put([FromBody]Device device)
         {
-            if (String.IsNullOrEmpty(device.DeviceGuid))
+            var deviceToSave = await Session.Query<Device>().FirstOrDefaultAsync(d => d.Token == device.Token);
+            if (deviceToSave == null)
             {
-                device.DeviceGuid = Guid.NewGuid().ToString();
+                deviceToSave = await Session.Query<Device>().FirstOrDefaultAsync(d => d.DeviceGuid == device.DeviceGuid);
+            }
+            if (deviceToSave == null)
+            {
+                deviceToSave = new Device {DeviceGuid = Guid.NewGuid().ToString()};
             }
 
-            device.TimeStamp = DateTime.UtcNow;
+            deviceToSave.TimeStamp = DateTime.UtcNow;
+            deviceToSave.UserName = device.UserName;
+            deviceToSave.SubscriptionCategories = device.SubscriptionCategories;
+            deviceToSave.PlatformDescription = device.PlatformDescription;
+            deviceToSave.Platform = device.Platform;
+            deviceToSave.Token = device.Token;
 
-            await Session.StoreAsync(device);
-            
-            //_devices[device.DeviceGuid] = device;
+            await Session.StoreAsync(deviceToSave);
 
             return Ok(device);
         }
@@ -51,7 +56,6 @@ namespace PushNotificationDemo.WebApiBackend.Controllers
         // DELETE api/device
         public IHttpActionResult Delete()
         {
-            //_devices.Clear();
             Session.DeleteAll<Device>();
             return Ok();
         }
