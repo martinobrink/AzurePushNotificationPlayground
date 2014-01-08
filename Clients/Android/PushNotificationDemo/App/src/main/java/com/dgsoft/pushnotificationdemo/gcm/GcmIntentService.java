f8abc6f8d829b464528e6cac312009e311827c8c
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-01-08 23:38:32 +0100

    android client: implemented display of message received by backend

--- Clients/Android/PushNotificationDemo/App/src/main/java/com/dgsoft/pushnotificationdemo/gcm/GcmIntentService.java
+++ Clients/Android/PushNotificationDemo/App/src/main/java/com/dgsoft/pushnotificationdemo/gcm/GcmIntentService.java
@@ -30,28 +30,30 @@ public class GcmIntentService extends IntentService{
         if (!extras.isEmpty()) {
 
             if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-                sendNotification("Send error: " + extras.toString());
+                //createNotification("Send error: " + extras.toString());
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
-                sendNotification("Deleted messages on server: " + extras.toString());
+                //createNotification("Deleted messages on server: " + extras.toString());
             } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                // A regular GCM message was received, if work needs to be done it can be done here
-                // e.g. deserialize json data received in extras
-                sendNotification("Received: " + extras.toString());
+                String message = extras.getString("message");
+                String senderName = extras.getString("senderName");
+                String recipientNames = extras.getString("recipientNames");
+                //String[] recipientList = recipientNames.split("#");
+                createNotification(message, senderName, recipientNames);
                 Log.i(TAG, "Received: " + extras.toString());
             }
         }
         GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
-    private void sendNotification(String msg) {
+    private void createNotification(String message, String senderName, String recipientNames) {
         notificationManager = (NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this)
-                        .setContentTitle("GCM Notification")
-                        .setContentText(msg)
-                        .setSubText("some sub text")
+                        .setContentTitle(senderName + " says:")
+                        .setContentText( "\"" + message + "\"")
+                        .setSubText("Recipients: " + recipientNames.replace("#", ","))
                         .setSmallIcon(R.drawable.ic_launcher)
-                        .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
                         .setContentIntent(contentIntent);
 
         notificationManager.notify(NOTIFICATION_ID, builder.build());
