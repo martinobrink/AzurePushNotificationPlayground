27d36ea7c8361dcf3be9f6c5736ea3b08c8ca9b4
Martin Öbrink-Hansen <martin@obrink-hansen.dk>
2014-02-16 21:19:13 +0100

    bumped gradle to 0.8 / 1.10 and buildtools to 19.0.1

--- Clients/Android/PushNotificationDemo/App/src/main/java/com/dgsoft/pushnotificationdemo/MainActivity.java
+++ Clients/Android/PushNotificationDemo/App/src/main/java/com/dgsoft/pushnotificationdemo/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 import com.dgsoft.pushnotificationdemo.backend.PushNotificationClient;
 import com.dgsoft.pushnotificationdemo.model.Device;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -150,7 +149,6 @@ public class MainActivity extends Activity {
             });
 
             Log.i(TAG, registrationId);
-            // this code will send registration id of a device to our own server.
         }
 
         private Device createDevice(String registrationId) {
--- Clients/Android/PushNotificationDemo/App/src/main/java/com/dgsoft/pushnotificationdemo/gcm/GcmBroadcastReceiver.java
+++ Clients/Android/PushNotificationDemo/App/src/main/java/com/dgsoft/pushnotificationdemo/gcm/GcmBroadcastReceiver.java
@@ -11,9 +11,9 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         // Explicitly specify that GcmIntentService will handle the intent.
-        ComponentName comp = new ComponentName(context.getPackageName(), GcmIntentService.class.getName());
+        ComponentName componentName = new ComponentName(context.getPackageName(), GcmIntentService.class.getName());
         // Start the service, keeping the device awake while it is launching.
-        startWakefulService(context, (intent.setComponent(comp)));
+        startWakefulService(context, (intent.setComponent(componentName)));
         setResultCode(Activity.RESULT_OK);
     }
 }
